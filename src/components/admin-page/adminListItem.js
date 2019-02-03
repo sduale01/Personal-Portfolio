4ad9de46d39b76378e34a67fa4450db23a59748c
Sharmarke Duale <sduale01@gmail.com>
2019-02-03 16:57:06 -0600

    add a delete button to each project in admin list item file

--- src/components/admin-page/adminListItem.js
+++ src/components/admin-page/adminListItem.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 
 class adminListItem extends Component {
+    deleteProject = () => {
+        console.log('delete pressed', this.props.project.id);
+        
+    }
     render() {
         return (
             <tr>
                 <td>{this.props.project.name}</td>
+                <td><button onClick={this.deleteProject}>DELETE</button></td>
             </tr>
         );
     }
