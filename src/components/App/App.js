44a1b4fe0f29f0cf80d8e4dc203a8aa8a28028d2
Sharmarke Duale <sduale01@gmail.com>
2019-02-03 16:33:14 -0600

    add the route to the first page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ProjectLists from '../project-page/projects-list.js';
 import Admin from '../admin-page/admin.js';
+import projectsList from '../project-page/projects-list.js';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
-        <Admin />
+        <Router>
+          <div>
+            <Route exact path="/" component={projectsList}/>
+          </div>
+        </Router>
+        {/* <Admin /> */}
         {/* <ProjectLists /> */}
       </div>
     );
